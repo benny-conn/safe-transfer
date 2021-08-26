8eb50e757e0213200c175c0027316501419d24a5
bennycio <benny@bennyc.io>
2021-08-26 12:00:46 -0700

    deploy in one script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,14 @@
 async function main() {
-  const Contract = await ethers.getContractFactory("SafeTransfer")
+  const SafeTransfer = await ethers.getContractFactory("SafeTransfer")
 
   // Start deployment, returning a promise that resolves to a contract object
-  const contract = await Contract.deploy() // Instance of the contract
-  console.log("Contract deployed to address:", contract.address)
+  const safeTransfer = await SafeTransfer.deploy() // Instance of the contract
+  console.log("SafeTransfer deployed to address:", safeTransfer.address)
+  const SafeTransferNFT = await ethers.getContractFactory("SafeTransferNFT")
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const safeTransferNFT = await SafeTransferNFT.deploy() // Instance of the contract
+  console.log("SafeTransferNFT deployed to address:", safeTransferNFT.address)
 }
 
 main()
